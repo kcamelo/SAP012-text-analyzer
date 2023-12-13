8b4b9d663e4090f2b6afb980aeae63945238f1d3
kcamelo <153109575+kcamelo@users.noreply.github.com>
2023-12-13 09:15:15 -0300

    13.12.2023_01

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 analyzer.getWordCount(text)
+
+analyzer.getCharacterCount (text)
+
+analyzer.getCharacterCountExcludingSpaces(text)
+
+analyzer.getNumberCount(text)
+
+analyzer.getNumberSum(text)
+
+analyzer.getAverageWordLength(text)
