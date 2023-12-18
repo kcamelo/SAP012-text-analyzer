8d42eacb54dbf651f2fd4f101a1447bf5bed977b
kcamelo <153109575+kcamelo@users.noreply.github.com>
2023-12-18 14:28:49 -0300

    18.12.2023_01

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import analyzer from './analyzer.js';
-
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-analyzer.getWordCount(text)
-
-analyzer.getCharacterCount (text)
-
-analyzer.getCharacterCountExcludingSpaces(text)
-
-analyzer.getNumberCount(text)
-
-analyzer.getNumberSum(text)
-
-analyzer.getAverageWordLength(text)
+console.log(document.getElementById("") )
 
 
+
+
+const textarea = document.querySelector('[name="user-input"]');
+const button = document.getElementById("reset-button");
+const copyButton = document.getElementById("copy-button");
+
+const wordCount = document.querySelector('[data-testid="word-count"]');
+const characterCount = document.querySelector('[data-testid="character-count"]');
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
+const numberCount = document.querySelector('[data-testid="number-count"]');
+const numberSum = document.querySelector('[data-testid="number-sum"]');
+
+textarea.addEventListener("keyup", allMetricsUpdate);
+button.addEventListener("click", clearAllMetricsAndText);
+copyButton.addEventListener("click", copyContent);
+
+function clearAllMetricsAndText() {
+  textarea.value = "";
+  wordCount.textContent = "Word count: 0";
+  characterCount.textContent = "Character count: 0";
+  characterNoSpacesCount.textContent = "Character no spaces count: 0";
+  numberCount.textContent = "Number: 0";
+  numberSum.textContent = "Number sum: 0";
+  wordLengthAverage.textContent = "Word length average: 0";
+}
+
+getAverageth
